4ee9656721e0eb327ac38ada4e70a95b8149daf9
TANMOY <119417251+TanmoyBD@users.noreply.github.com>
2023-01-22 19:52:18 +0600

    Add files via upload

---
+++ Module 7 Theory Assignment 01/problem 4.c
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+
+    getline(cin,st);
+
+    for (int i=0;i<st.length();i++)
+    {
+        if (st[i]=='z')
+        {
+            st[i]='a';
+            continue;
+        }
+        if (i%2==0)
+        {
+            st[i]=st[i]+1;
+        }
+    }
+    cout<<st;
+
+    return 0;
+}
